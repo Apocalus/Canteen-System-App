3bf849c38d046545a2311a1e85854343bd35583c
Rabeea Salman <59884290+Apocalus@users.noreply.github.com>
2024-01-24 21:37:58 +0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_connection/extras/crud_operations.dart';
+import 'package:firebase_connection/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+import 'firebase_options.dart';
+import 'screens/homeScreen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple, background: Colors.white),
+        useMaterial3: true,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
